bdeb3830d1ada40fdd95c385d562f4e0edf26522
buddy <litoubuddy@gmail.com>
2023-08-27 09:34:12 +0800

    lint: format babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,34 +1,35 @@
 module.exports = {
-	presets: ['module:metro-react-native-babel-preset'],
-	plugins: [
-		[
-			'import',
-			{
-				libraryName: 'lodash',
-				libraryDirectory: '',
-				camel2DashComponentName: false,
-			},
-		],
-		[
-			'module-resolver',
-			{
-				root: ['./src/'],
-				extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-				alias: {
-					tests: ['./tests/'],
-					'@/apis': ['./src/apis'],
-					'@/assets': ['./src/assets'],
-					'@/components': ['./src/components'],
-					'@/constants': ['./src/constants'],
-					'@/hooks': ['./src/hooks'],
-					'@/locales': ['./src/locales'],
-					'@/pages': ['./src/pages'],
-					'@/routers': ['./src/routers'],
-					'@/store': ['./src/store'],
-					'@/utils': ['./src/utils'],
-				},
-			},
-		],
-		'react-native-reanimated/plugin',
-	],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: 'lodash',
+        libraryDirectory: '',
+        camel2DashComponentName: false,
+      },
+    ],
+    [
+      'module-resolver',
+
+      {
+        root: ['./src/'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@/apis': ['./src/apis'],
+          '@/assets': ['./src/assets'],
+          '@/components': ['./src/components'],
+          '@/constants': ['./src/constants'],
+          '@/hooks': ['./src/hooks'],
+          '@/locales': ['./src/locales'],
+          '@/pages': ['./src/pages'],
+          '@/routers': ['./src/routers'],
+          '@/store': ['./src/store'],
+          '@/utils': ['./src/utils'],
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
