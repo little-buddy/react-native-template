75077cec52fbe598b12f31db22df089b6a8eca93
buddy <litoubuddy@gmail.com>
2023-08-27 14:17:18 +0800

    chore: update docs

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import appJson from './app.json';
+/**
+ * There is no additional configuration required for the web, however,
+ * since the Gesture Handler 2.10.0 the new web implementation is enabled by default
+ * @see https://docs.swmansion.com/react-native-gesture-handler/docs/installation
+ *  */
+// import { enableLegacyWebImplementation } from 'react-native-gesture-handler';
+// enableLegacyWebImplementation(true); <2.6.0
+// enableExperimentalWebImplementation(true); >= 2.6.0 <= 2.10.0
 
 const appName = appJson.name;
 
