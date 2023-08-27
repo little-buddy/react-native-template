1665684bd508ca8643e5a139d121b441e101bc1c
buddy <litoubuddy@gmail.com>
2023-08-27 17:42:29 +0800

    chore: wrapper with GestureHandlerRootView

--- App.tsx
+++ App.tsx
@@ -8,11 +8,13 @@
 import { StatusBar, useColorScheme } from 'react-native';
 import { Provider as ReactReduxProvider } from 'react-redux';
 // @ts-ignore
-import { PersistGate } from 'redux-persist/integration/react';
-import 'react-native-devsettings';
 import { NavigationContainer } from '@react-navigation/native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import 'react-native-devsettings';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import RootComponent from '@/routers';
 import { persistor, store } from '@/store';
@@ -28,14 +30,15 @@ function App(): JSX.Element {
     <SafeAreaProvider>
       <ReactReduxProvider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <StatusBar
-            barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-            backgroundColor={backgroundStyle.backgroundColor}
-          />
-
-          <NavigationContainer>
-            <RootComponent />
-          </NavigationContainer>
+          <GestureHandlerRootView>
+            <StatusBar
+              barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+              backgroundColor={backgroundStyle.backgroundColor}
+            />
+            <NavigationContainer>
+              <RootComponent />
+            </NavigationContainer>
+          </GestureHandlerRootView>
         </PersistGate>
       </ReactReduxProvider>
     </SafeAreaProvider>
