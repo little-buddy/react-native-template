452e693e295a0d117c57e7fcacf33d065c5fe245
buddy <litoubuddy@gmail.com>
2023-08-27 14:02:36 +0800

    fixed: babel plugin with react-native-reanimated

--- config-overrides.js
+++ config-overrides.js
@@ -9,20 +9,24 @@ const path = require('path');
 
 const babelrc = require('./babel.config.js');
 
+// @deprecated
 const addWebpackIgnoreWarnings = () => config => {
   config.ignoreWarnings = [/only default export is available soon/];
-
   return config;
 };
 
 module.exports = override(
   addWebpackIgnoreWarnings(),
-  addBabelPlugins(...babelrc.plugins, 'react-native-web'),
+  addBabelPlugins(
+    ...babelrc.plugins.slice(0, babelrc.plugins.length - 1),
+    'react-native-web',
+    '@babel/plugin-proposal-export-namespace-from',
+    babelrc.plugins[babelrc.plugins.length - 1]
+  ),
   addBabelPresets([
     'module:metro-react-native-babel-preset',
     // @see https://github.com/nrwl/nx/issues/14407#issuecomment-1439327945
     { useTransformReactJSXExperimental: true },
   ]),
-  useBabelRc(),
   removeModuleScopePlugin()
 );
